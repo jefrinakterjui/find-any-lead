0b15d76983f888ac7e41d82c657aa2ecb82b4542
JefrinAkterJui <jefrin.jui.dev@gmail.com>
2025-11-26 20:17:35 +0600

    logo

--- src/components/shared/Navbar/logo.tsx
+++ src/components/shared/Navbar/logo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export const Logo = () => (
-  <Image width={90} height={90} src="/logo.jpg" alt="Company Logo"></Image>
+  <Image width={100} height={100} className="w-28" src="/logo.jpg" alt="Company Logo"></Image>
 );
